b7c273c2f243fafc0b7e1f3febc4e9d5e194538d
campbell88 <shatharcampbell@gmail.com>
2017-04-12 13:42:02 -0400

    first commit - the inputted number produces the squared and cubed numbers from 1 up to the value entered

---
+++ LabNumber3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please enter an integer: ");
+            int number = int.Parse(Console.ReadLine());
+        
+
+
+            for (int i=1; i <= number; i++)
+
+            {
+            double squared = Math.Pow(i, 2);
+            double cubed = Math.Pow(i, 3);
+            Console.WriteLine("For Number {0}\n", i);
+            Console.WriteLine("Your number squared is {0}\n", squared);
+            Console.WriteLine("Your number cubed is {0}\n", cubed);
+
+            
+
+
+            }
+
+
+
+
+        }
+    }
+}
