4c91b4d7b3d0b76f7a51bce2c7ef2d3d324cbf55
campbell88 <shatharcampbell@gmail.com>
2017-04-12 14:16:02 -0400

    added user prompt to continue

--- LabNumber3/Program.cs
+++ LabNumber3/Program.cs
@@ -8,31 +8,37 @@ namespace LabNumber3
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            bool askToContinue = false; //do while to ask user to continue
+            do
+           {
 
-            Console.Write("Please enter an integer: ");
-            int number = int.Parse(Console.ReadLine());
-        
+                Console.Write("Please enter an integer: ");
+                int number = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= number; i++) //counts up to value entered
+
+                {
+                    double squared = Math.Pow(i, 2); //value squared
+                    double cubed = Math.Pow(i, 3); //value cubed
+                    Console.WriteLine("For Number {0}\n", i);
+                    Console.WriteLine("Your number squared is {0}\n", squared);
+                    Console.WriteLine("Your number cubed is {0}\n", cubed);
+
+    
 
 
-            for (int i=1; i <= number; i++)
+                }
+                Console.WriteLine("Type YES to continue");
 
-            {
-            double squared = Math.Pow(i, 2);
-            double cubed = Math.Pow(i, 3);
-            Console.WriteLine("For Number {0}\n", i);
-            Console.WriteLine("Your number squared is {0}\n", squared);
-            Console.WriteLine("Your number cubed is {0}\n", cubed);
+                   if (Console.ReadLine().ToUpper() == "YES")
+                        askToContinue = true;
 
-            
-
+                } while (askToContinue); //added boolean expression to prompt user to continue (will need to update it to y/n option and include validation once I improve)
 
             }
-
-
-
-
-        }
+        
     }
 }
